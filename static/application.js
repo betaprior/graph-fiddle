4356bd297228d18f2d133d8bd238d3a42a28c563
Leo Alekseyev <leo.alekseyev+git.mbp@gmail.com>
2014-04-23 00:47:35 -0700

    Dijkstra animation works

--- static/application.js
+++ static/application.js
@@ -1,5 +1,6 @@
 var points = [];
 var width = 960, height = 500;
+var nodeR = 20;
 var svg;
 
 function placeSVG() {
@@ -68,8 +69,8 @@ function prepSvg(width, height) {
 		.enter().append("svg:marker")    // This section adds in the arrows
 		.attr("id", String)
 		.attr("viewBox", "0 -5 10 10")
-		.attr("refX", 15)
-		.attr("refY", -1.5)
+		.attr("refX", 7)
+		.attr("refY", 0)
 		.attr("markerWidth", 6)
 		.attr("markerHeight", 6)
 		.attr("orient", "auto")
@@ -112,10 +113,12 @@ function renderGraph(graph) {
 		.call(force.drag);
 	
 	// add nodes
-	node.append("circle").attr("r", 20);
+	node.append("circle").attr("r", nodeR);
 	// add weights
 	node.append("text")
 		.attr("class", "dist")
+		.attr("text-anchor", "middle")	
+		.attr("dy", "3px")
 		.attr("x", 0)
 		.text(function(d) { return d.dist; });
 
@@ -131,12 +134,17 @@ function renderGraph(graph) {
 			var dx = d.target.x - d.source.x,
             dy = d.target.y - d.source.y,
             dr = Math.sqrt(dx * dx + dy * dy);
+			var rhat_x = 1.5 * dx / dr, rhat_y = 1.5 * dy / dr;
+			var sx = d.source.x + nodeR * rhat_x,
+			sy = d.source.y + nodeR * rhat_y,
+			tx = d.target.x - nodeR * rhat_x,
+			ty = d.target.y - nodeR * rhat_y;
 			return "M" + 
-				d.source.x + "," + 
-				d.source.y + "A" + 
+				sx + "," + 
+				sy + "A" + 
 				dr + "," + dr + " 0 0,1 " + 
-				d.target.x + "," + 
-				d.target.y;
+				tx + "," + 
+				ty;
 		});
 
 		node.attr("transform", function(d) { 
@@ -150,6 +158,30 @@ function renderGraph(graph) {
 		force.tick();
 		k = k + 1;
 	}	
+}
+
+function displayNodeDist(node, style, cls) {
+	var sel = d3.selectAll("#node" + node.name).select("text");
+	sel.text(function(d) { 
+		return d.dist === Infinity ? '∞' : d.dist;
+	});
+	if (style) { sel.style(style); }
+	if (cls) { sel.classed(cls, true); }
+}
+
+function showNodeDist(node, dist, cls) {
+	var sel = d3.selectAll("#node" + node.name).select("text");
+	sel.text(function(d) { return dist === Infinity ? '∞' : dist; });
+	if (cls) { sel.classed(cls, true); }
+}
+
+function initializeDistViz(graph, source) {
+	_(graph.nodes).each(function(x) {
+		x.dist = Infinity;
+		displayNodeDist(x);
+	});
+	graph.nodes[source].dist = 0;
+	displayNodeDist(graph.nodes[source]);
 }
 
 function initializeDist(graph, source) {
@@ -171,8 +203,8 @@ function relax(edge) {
 function relaxViz(edge) {
 	edge.target.dist = edge.source.dist + edge.value;
 	var d = edge.target.dist;
-	d3.selectAll("#node" + edge.target.name).select("text")
-		.text(function(d) { return d.dist; });
+	// displayNodeDist(edge.target, {"font-weight": "bold", "fill": "green"});
+	displayNodeDist(edge.target, null, "relaxing");
 	return d;
 }
 
@@ -196,47 +228,16 @@ function makeIndexedPQ() {
 }
 
 function runBellmanFord(graph, source, target) {
-	var countV = 0;
-	var timeout = 100;
-	var edges = d3.values(graph.edges);
-	var doVertex = function() {
-		console.log("Doing vertex " + countV);
-		countV++;
-		doEdge(0);
-	};
-	var doEdge = function(i, prev) {
-		console.log("doing edge " + i);
-		d3.select("#link" + edges[i].id)
-			.classed("visiting", true);
-		if (prev) {
-			d3.select("#link" + edges[prev].id)
-				.classed("visiting", false);
-		}
-		if (isTense(edges[i])) { relax(edges[i]); }
-		if (i == graph.E - 1) {
-			if (countV == graph.V - 1) {
-				return;
-			} else {
-				setTimeout(doVertex, timeout);
-			}
-		} else {
-			setTimeout(function() { doEdge(i + 1, i); }, timeout);
-		}
-	};
-		
-
-			
 	initializeDist(graph, source);
-	// _(graph.nodes).each(function(v, k) {
-	// 	_(graph.edges).each(function(edge, k) {
-	// 		if (isTense(edge)) { relax(edge); }
-	// 	});
-	// });
-	doVertex();
+	_(graph.nodes).each(function(v, k) {
+		_(graph.edges).each(function(edge, k) {
+			if (isTense(edge)) { relax(edge); }
+		});
+	});
 	console.log("At the end of BF, source->target dist is " + graph.nodes[target].dist);
 }
 
-function runBellmanFordViz(graph, source, target) {
+function runBellmanFordViz1(graph, source, target) {
 	var countV = 0;
 	var timeout = 100;
 	var edges = d3.values(graph.edges);
@@ -264,18 +265,23 @@ function runBellmanFordViz(graph, source, target) {
 			setTimeout(function() { doEdge(i + 1, i); }, timeout);
 		}
 	};
+	doVertex();
+}
+
+function runBellmanFordViz(graph, source, target) {
+	var timeout = 1000;
 		
 	var actions = [];
-	var prevV = null, prevE = null;
-	initializeDist(graph, source);
+	initializeDistViz(graph, source);
 	_(graph.nodes).each(function(vtx, name) {
 		_(graph.edges).each(function(edge, id) {
 			var action = function(edge, id) {
 				return function() {
 					// console.log("Visiting link with id " + id);
-					if (isTense(edge)) { relaxViz(edge); }
 					d3.selectAll("[id^=link]").classed("visiting", false);
+					d3.selectAll("text.dist").classed("relaxing", false);
 					d3.selectAll("#link" + id).classed("visiting", true);
+					if (isTense(edge)) { relaxViz(edge); }
 				};
 			};
 			actions.push(action(edge, id));
@@ -323,13 +329,69 @@ function runDijkstra(graph, source, target) {
 	console.log("At the end of Dijkstra, source->target dist is " + graph.nodes[target].dist);
 }
 
+function runDijkstraViz(graph, source, target) {
+	initializeDistViz(graph, source);
+	var actions = [];
+	var timeout = 1000;
+	var pq = makeIndexedPQ();
+	var node = graph.nodes[source];
+	node.pqHandle = pq.push(node, node.dist);
+	while (!pq.isEmpty()) {
+		var curNode = pq.deleteMin();
+		curNode.pqHandle = null;
+		for (var i = 0; i < curNode.adj.length; i++) {
+			var edge = graph.links[curNode.adj[i]];
+			var newDist;
+			var step = {edge: edge, newDist: newDist, relaxing: false};
+			if (isTense(edge)) {
+				step.relaxing = true;
+				newDist = relax(edge);
+				step.newDist = newDist;
+				if (edge.target.pqHandle) {
+					pq.changeKey(pqHandle, newDist);
+				} else {
+					edge.target.pqHandle = pq.push(edge.target, newDist);
+				}
+			}
+			actions.push(step);
+		}
+	}
+	var vizStep = function(step) {
+		d3.selectAll("[id^=link]").classed("visiting", false);
+		d3.selectAll("text.dist").classed("relaxing", false);
+		d3.selectAll("#link" + step.edge.id).classed("visiting", true);
+		if (step.relaxing) {
+			showNodeDist(step.edge.target, newDist, "relaxing");
+		}
+	};
+	
+	var deselectAll = function() {
+		d3.selectAll("[id^=link]").classed("visiting", false);
+		d3.selectAll("text.dist").classed("relaxing", false);
+	};
+	
+	var runActions = function(i) {
+		i = i || 0;
+		vizStep(actions[i]);
+		if (i < actions.length - 1)
+			setTimeout(function() { runActions(i+1); }, timeout);
+		else
+			setTimeout(deselectAll, timeout);
+	};
+	
+	runActions();
+		
+	console.log("At the end of Dijkstra, source->target dist is " + graph.nodes[target].dist);
+}
+
+
 $(document).ready(function() {
 	
 	getData("files/a4.txt")
 		.done(function(graph) {
 			renderGraph(graph);
-			runBellmanFordViz(graph, "0", "1");
-			// runDijkstra(graph, "0", "1");
+			// runBellmanFordViz(graph, "0", "1");
+			runDijkstraViz(graph, "0", "1");
 		});
 	// var foo = makeIndexedPQ();
 	// foo.push("a", 10);
